b75afa65a997ddab120e52aa5099cd772a0d6ea9
igormarims <igormarquespe@gmail.com>
2023-04-08 14:40:39 -0300

    Criando Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 
 import './App.css'
+import  { Home } from './pages/Home'
+import  { Sobre } from './pages/Sobre'
+import { Route, Routes  } from 'react-router-dom';
 
 function App() {
   
   return (
-    <div >
-      ...
+    <div className='py-4'>
+      <header>
+        <h1>Título do site </h1>
+      </header>
+      <hr />
+      <div className="py-4">
+        <Routes>
+           <Route path='/' element={ <Home/>}/>
+           <Route path='/sobre' element={ <Sobre/>}/>
+        </Routes>
+      </div>
+       <hr />
+       <footer>
+          todos os direitos reservados.
+       </footer>
     </div>
+
+
   )
 }
 
